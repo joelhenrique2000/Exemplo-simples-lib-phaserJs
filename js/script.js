b901fb804789cb0f7f74994a288da7b4f662d219
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-06-23 00:39:07 -0300

    adicionando movimentação e a animação do sprite

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 	
 	// Instaciamento do "jogo"
 		var game = new Phaser.Game(800,600,Phaser.AUTO,null,{preload:preload,create:create,update:update});
-		var platforms, player;
+		var platforms, player, keys;
 		
 	// carregamento de recursos
 	function preload() {
@@ -14,8 +14,9 @@
 
 	// criação dos elementos do jogo(no contexto do jogo)
 	function create() {
-		game.physics.startSystem(Phaser.Physics.ARCADE);
 		
+		keys = game.input.keyboard.createCursorKeys();
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.add.sprite(0,0,'sky');
 
 		platforms = game.add.group();
@@ -26,19 +27,42 @@
 			platform.body.immovable = true;
 			
 			platform = platforms.create(400, 400, 'platform');
+			platform.body.immovable = true;
 			
 			platform = platforms.create(-150, 250, 'platform');
+			platform.body.immovable = true;
 			
 		player = game.add.sprite(50, game.world.height - 150, 'dude');
 		game.physics.arcade.enable(player);
 		player.body.gravity.y = 300;
 		player.body.bounce.y = 0.2;
 		player.body.collideWorldBounds = true;
+		player.animations.add('left', [0,1,2,3], 10, true);
+		player.animations.add('right', [5,6,7,8], 10, true);
 	}
 
 	// regras do jogos
 	function update() {
 		game.physics.arcade.collide(player,platforms);
+		
+		player.body.velocity.x = 0;
+		if(keys.left.isDown) {
+			player.body.velocity.x = -150;
+			player.animations.play('left');
+		} else if(keys.right.isDown) {
+			player.body.velocity.x = 150;
+			player.animations.play('right');
+		} else {
+			player.animations.stop();
+			player.frame = 4;
+		}
+		
+		if(keys.up.isDown && player.body.touching.down) {
+			player.body.velocity.y = -350;
+		} else if(keys.down.isDown) {
+			player.body.velocity.y = 165;
+		}
+			
 	}
 
 
