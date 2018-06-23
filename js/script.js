b7124cabf45e8db1684cc2124d4bc49be859010d
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-06-23 00:58:43 -0300

    adicionando o placa do score

--- js/script.js
+++ js/script.js
@@ -33,14 +33,6 @@
 			platform = platforms.create(-150, 250, 'platform');
 			platform.body.immovable = true;
 			
-		player = game.add.sprite(50, game.world.height - 150, 'dude');
-		game.physics.arcade.enable(player);
-		player.body.gravity.y = 300;
-		player.body.bounce.y = 0.2;
-		player.body.collideWorldBounds = true;
-		player.animations.add('left', [0,1,2,3], 10, true);
-		player.animations.add('right', [5,6,7,8], 10, true);
-		
 		stars = game.add.group();
 		stars.enableBody = true;
 		
@@ -49,6 +41,16 @@
 				star.body.gravity.y = 300;
 				star.body.bounce.y = 0.7 + (Math.random()*0.2);
 		}
+		
+		player = game.add.sprite(50, game.world.height - 150, 'dude');
+		game.physics.arcade.enable(player);
+		player.body.gravity.y = 300;
+		player.body.bounce.y = 0.2;
+		player.body.collideWorldBounds = true;
+		player.animations.add('left', [0,1,2,3], 10, true);
+		player.animations.add('right', [5,6,7,8], 10, true);
+		
+		txtScore = game.add.text(16, 16, 'SCORE: 0', {fontSize: '32px', fill: '#FFFFFF'});
 	}
 
 	// regras do jogos
