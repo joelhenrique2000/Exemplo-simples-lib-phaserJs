8703e91079150bf70879010d2cd6301b5eb5c8ff
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-06-23 00:00:02 -0300

    add as plataformas que vão ser o chão

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 	
 	// Instaciamento do "jogo"
 		var game = new Phaser.Game(800,600,Phaser.AUTO,null,{preload:preload,create:create,update:update});
-
+		var platforms;
+		
 	// carregamento de recursos
 	function preload() {
 		game.load.image('sky','img/sky.png');
@@ -12,6 +13,11 @@
 	// criação dos elementos do jogo(no contexto do jogo)
 	function create() {
 		game.add.sprite(0,0,'sky');
+
+		platforms = game.add.group();
+		
+		var platform = platforms.create(0, game.world.height - 64, 'platform');
+			platform.scale.setTo(2,2);
 	}
 
 	// regras do jogos
